2eef023a9c97608d831ba28e3abd470779787778
Ralf Donau <ruelle@volleyballschlaeger.de>
2023-01-05 18:53:11 +0100

    ittree: all_iters()

--- ittree.py
+++ ittree.py
@@ -4,3 +4,11 @@ def flat(iterable):
 			yield from flat(i)
 		except TypeError:
 			yield i
+
+def all_iters(iterable):
+	for i in iterable:
+		try:
+			yield from all_iters(i)
+			yield i
+		except TypeError:
+			pass
